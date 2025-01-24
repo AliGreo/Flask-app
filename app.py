df33bb652f08c9812094f72c37a626fd9c420798
aligreo <agreu77@yahoo.com>
2025-01-24 09:57:28 +0200

    updated version 1

--- app.py
+++ app.py
@@ -4,4 +4,29 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-db = SQLAlchemy(app)
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/calculate', methods=['POST','GET'])
+def calculate():
+     
+    result = None
+
+    if request.method == 'POST':
+        first_number = request.form.get('num1')
+        second_number = request.form.get('num2')
+
+        print(first_number, second_number)
+        
+        result = int(first_number) + int(second_number)
+
+    return render_template('calculate.html', result=result)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
